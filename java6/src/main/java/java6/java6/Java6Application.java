451f7a5005d8d4c2fa829565d58c6d96a3d62bfb
Zudeptrai <165445911+Zudeptrai@users.noreply.github.com>
2025-03-17 23:13:28 +0700

    ngay dau
    ket nối sql

---
+++ java6/src/main/java/java6/java6/Java6Application.java
@@ -0,0 +1,13 @@
+package java6.java6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java6Application.class, args);
+	}
+
+}
